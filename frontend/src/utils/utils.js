36130042d2ee07ec4097cc637923b05fca3d19cb
LinneaWa <linnea.wahlstrom@gmail.com>
2023-06-07 12:15:41 +0200

    new backend server

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,4 @@
 
-export const PHOTO_URL ="https://filip-foto-film-final-project-dec22-gqk64wdp4q-lz.a.run.app/photos";
+export const PHOTO_URL ="https://filipfotofilmbackend.onrender.com/photos";
 
-export const VIDEO_URL ="https://filip-foto-film-final-project-dec22-gqk64wdp4q-lz.a.run.app/videos";
+export const VIDEO_URL ="https://filipfotofilmbackend.onrender.com/videos";
